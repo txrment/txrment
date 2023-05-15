a3ac9d638d6841bd168a7abdbb1e5bfcc428361d
Паша Воронов <paha1.nosik34@mail.ru>
2023-05-15 15:11:21 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp19
+{
+    public static class ClassFrame1
+    {
+        static public Frame frame1 = new Frame();
+    }
+}
---
+++ Page1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp19
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("Page2.xaml", UriKind.Relative));
+        }
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("Page3.xaml", UriKind.Relative));
+        }
+    }
+}
